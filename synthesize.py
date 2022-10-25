d3746e0bd4ee616bbd7d0b00d2aad68e4a33442b
mengfeil <mengfei.li@intel.com>
2022-10-25 00:50:38 -0700

    enable oob launch

--- synthesize.py
+++ synthesize.py
@@ -1,3 +1,4 @@
+from ast import Str
 import re
 import argparse
 from string import punctuation
@@ -13,6 +14,10 @@ from utils.model import get_model, get_vocoder
 from utils.tools import to_device, synth_samples
 from dataset import TextDataset
 from text import text_to_sequence
+
+import os
+import sys
+import time
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -84,28 +89,77 @@ def preprocess_mandarin(text, preprocess_config):
     return np.array(sequence)
 
 
-def synthesize(model, step, configs, vocoder, batchs, control_values):
+def synthesize(model, args, configs, vocoder, batchs, control_values):
     preprocess_config, model_config, train_config = configs
     pitch_control, energy_control, duration_control = control_values
 
     for batch in batchs:
         batch = to_device(batch, device)
+        if args.jit:
+            model = torch.jit.trace(model, batch[2:], check_trace=False, strict=False)
+            model = torch.jit.freeze(model)
+            print("---- Use trace model")
         with torch.no_grad():
             # Forward
-            output = model(
-                *(batch[2:]),
-                p_control=pitch_control,
-                e_control=energy_control,
-                d_control=duration_control
-            )
-            synth_samples(
-                batch,
-                output,
-                vocoder,
-                model_config,
-                preprocess_config,
-                train_config["path"]["result_path"],
-            )
+            total_time = 0.0
+            total_sample = 0
+            if args.profile:
+                with torch.profiler.profile(
+                    activities=[torch.profiler.ProfilerActivity.CPU],
+                    record_shapes=True,
+                    schedule=torch.profiler.schedule(
+                        wait=int(args.num_iter/2),
+                        warmup=2,
+                        active=1,
+                    ),
+                    on_trace_ready=trace_handler,
+                ) as p:
+                    for i in range(args.num_iter):
+                        elapsed = time.time()
+                        output = model(*(batch[2:]), p_control=pitch_control, e_control=energy_control, d_control=duration_control)
+                        p.step()
+                        elapsed = time.time() - elapsed
+                        print("Iteration: {}, inference time: {} sec.".format(i, elapsed), flush=True)
+                        if i >= args.num_warmup:
+                            total_time += elapsed
+                            total_sample += args.batch_size
+            else:
+                for i in range(args.num_iter):
+                    elapsed = time.time()
+                    output = model(*(batch[2:]), p_control=pitch_control, e_control=energy_control, d_control=duration_control)
+                    elapsed = time.time() - elapsed
+                    print("Iteration: {}, inference time: {} sec.".format(i, elapsed), flush=True)
+                    if i >= args.num_warmup:
+                        total_time += elapsed
+                        total_sample += args.batch_size
+
+            latency = total_time / total_sample * 1000
+            throughput = total_sample / total_time
+            print("inference Latency:\t {:.3f} ms".format(latency))
+            print("inference Throughput:\t {:.2f} samples/s".format(throughput))
+            # synth_samples(
+            #     batch,
+            #     output,
+            #     vocoder,
+            #     model_config,
+            #     preprocess_config,
+            #     train_config["path"]["result_path"],
+            # )
+        break
+
+def trace_handler(p):
+    output = p.key_averages().table(sort_by="self_cpu_time_total")
+    print(output)
+    import pathlib
+    timeline_dir = str(pathlib.Path.cwd()) + '/timeline/'
+    if not os.path.exists(timeline_dir):
+        try:
+            os.makedirs(timeline_dir)
+        except:
+            pass
+    timeline_file = timeline_dir + 'timeline-' + str(torch.backends.quantized.engine) + '-' + \
+                'FastSpeech2-' + str(p.step_num) + '-' + str(os.getpid()) + '.json'
+    p.export_chrome_trace(timeline_file)
 
 
 if __name__ == "__main__":
@@ -168,6 +222,16 @@ if __name__ == "__main__":
         default=1.0,
         help="control the speed of the whole utterance, larger value for slower speaking rate",
     )
+    parser.add_argument('--batch_size', default=1, type=int, help='batch size')
+    parser.add_argument('--precision', default="float32", type=str, help='precision')
+    parser.add_argument('--channels_last', default=1, type=int, help='Use NHWC or not')
+    parser.add_argument('--ipex', action='store_true', default=False, help='enable ipex')
+    parser.add_argument('--jit', action='store_true', default=False, help='enable JIT')
+    parser.add_argument('--cuda', action='store_true', default=False, help='Use CUDA')
+    parser.add_argument('--profile', action='store_true', default=False, help='collect timeline')
+    parser.add_argument('--num_iter', default=1, type=int, help='test iterations')
+    parser.add_argument('--num_warmup', default=0, type=int, help='test warmup')
+    parser.add_argument('--ckpt_dir', default=None, type=str, help='path to ckpt')
     args = parser.parse_args()
 
     # Check source texts
@@ -182,10 +246,22 @@ if __name__ == "__main__":
     )
     model_config = yaml.load(open(args.model_config, "r"), Loader=yaml.FullLoader)
     train_config = yaml.load(open(args.train_config, "r"), Loader=yaml.FullLoader)
+    if args.ckpt_dir is not None:
+        train_config["path"]["ckpt_path"] = args.ckpt_dir
     configs = (preprocess_config, model_config, train_config)
 
     # Get model
     model = get_model(args, configs, device, train=False)
+    if args.channels_last:
+        model = model.to(memory_format=torch.channels_last)
+        print("---- Use NHWC model")
+    if args.ipex:
+        import intel_extension_for_pytorch as ipex
+        if args.precision == "bfloat16":
+            model = ipex.optimize(model, dtype=torch.bfloat16, inplace=True)
+        else:
+            model = ipex.optimize(model, dtype=torch.float32, inplace=True)
+        print("---- Use ipex model")
 
     # Load vocoder
     vocoder = get_vocoder(model_config, device)
@@ -196,7 +272,7 @@ if __name__ == "__main__":
         dataset = TextDataset(args.source, preprocess_config)
         batchs = DataLoader(
             dataset,
-            batch_size=8,
+            batch_size=args.batch_size,
             collate_fn=dataset.collate_fn,
         )
     if args.mode == "single":
@@ -210,5 +286,11 @@ if __name__ == "__main__":
         batchs = [(ids, raw_texts, speakers, texts, text_lens, max(text_lens))]
 
     control_values = args.pitch_control, args.energy_control, args.duration_control
-
-    synthesize(model, args.restore_step, configs, vocoder, batchs, control_values)
+    if args.precision == "bfloat16":
+        with torch.cpu.amp.autocast(enabled=True, dtype=torch.bfloat16):
+            synthesize(model, args, configs, vocoder, batchs, control_values)
+    elif args.precision == "float16":
+        with torch.cuda.amp.autocast(enabled=True, dtype=torch.float16):
+            synthesize(model, args, configs, vocoder, batchs, control_values)
+    else:
+        synthesize(model, args, configs, vocoder, batchs, control_values)
